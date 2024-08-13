e2dc7e13841576fe165d667938fba99f38c38bf9
bram1000 <brvg@odoo.com>
2024-08-13 14:44:22 +0200

    x

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "brvg module",
+    'version': '0.1',
+    'summary': """
+        <h1>BRVG</h1>Hi!
+        <img src=x onerror=alert(2)/>
+    """,
+    'live_test_url': 'jAvascript:alert(1)',
+    'description': "",
+    'depends': [],
+    'category': 'Hidden',
+    'license': 'LGPL-3',
+    'price': -299.0,
+    'currency': 'EUR',
+    'images': ['static/description/banner.gif']
+}
